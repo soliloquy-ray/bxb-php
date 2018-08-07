2887a84f0c2a9f9d361c585267b896a0ad5fa2f3
DESKTOP-5KE3RI9\Ray <santos.ray.rommel@gmail.com>
2018-08-07 17:33:13 +0800

    push api and test scripts

--- api.php
+++ api.php
@@ -7,8 +7,9 @@ if ($http_origin == "http://localhost:8100" || $http_origin == "https://bxb-app.
     header("Access-Control-Allow-Headers: 'Origin, X-Requested-With, Content-Type, Accept'");
 }
 
-
-include_once "index.php";
+require_once("index.php");
+require_once("mail.php");
+require_once("twilio.php");
 
 $req = $_REQUEST['q'];
 $p = json_decode(file_get_contents('php://input'), true);
@@ -21,14 +22,42 @@ function login($username = '', $password = '', $out = ''){
 	$sth->bindParam(2, $password);
 	$sth->execute();
 	
-	$arr = array();
-	if($i = $sth->fetch(PDO::FETCH_ASSOC)){
+	try{
+		$i = $sth->fetch(PDO::FETCH_ASSOC);
 		return $i['master_id'];
-	}
-	else{
+	}catch(Exception $e){
 		return false;
 	}
 
+}
+
+/**
+@master_id int,
+@principal float,
+@interest float,
+@paydays int,
+@purpose varchar(1000),
+@applicationDate datetime,
+@processFund float,
+@collectionFund float,
+@documentFee float   **/
+function addLoan($loan){
+	$dt = date('Y-m-d H:i:s');
+	$db = DB::getInstance();
+	$conn = $db->getConnection();
+	$sth = $conn->prepare("EXEC uspLoanApply ?, ?, ?, ?, ?, ?, ?, ?, ? ");
+	$sth->bindParam(1, $loan['id']);
+	$sth->bindParam(2, $loan['principal']);
+	$sth->bindParam(3, $loan['interest']);
+	$sth->bindParam(4, $loan['paydays']);
+	$sth->bindParam(5, $loan['purpose']);
+	$sth->bindParam(6, $dt);
+	$sth->bindParam(7, $loan['processFund']);
+	$sth->bindParam(8, $loan['collectionFund']);
+	$sth->bindParam(9, $loan['documentFee']);
+	$sth->execute();
+	
+	var_dump($sth->rowCount());
 }
 
 function getUserDetailsById($id = 0){
@@ -43,7 +72,7 @@ function getUserDetailsById($id = 0){
 		$arr[] = $i;
 	}
 
-	echo json_encode($arr);
+	return $arr[0];
 }
 
 function addUser($user = array()){
@@ -86,15 +115,86 @@ function getUserByTIN($tin = 0, $birth = '0000-00-00', $ccode = ''){
 	echo !empty($arr) ? json_encode($arr) : json_encode(array(false));
 }
 
+function changePass($id,$pass,$newPass){
+	$db = DB::getInstance();
+	$conn = $db->getConnection();
+
+	$sth = $conn->prepare("uspUserUpdatePassword ?, ?, ?"); // AND Company = ? 
+	$sth->bindParam(1, $id);
+	$sth->bindParam(2, $pass);
+	$sth->bindParam(3, $newPass);
+	$res = $sth->execute();
+
+	echo json_encode($res);
+}
+
+function sendMailForgotPw($email){
+
+	$api = base64_decode("U0cuREpjQkU3NEdUTC1PcHBXVHRjZTJFdy5qd3h0bHI0YURWRXVDUDM1ci1KMm16ZEFDLUlxaE4xd1p5QkNfUWxWdHpJ");
+	$mail = Mail::getInstance($api);
+
+	$succ = $mail->send('rsantos@bxbesc.com',$email,'Reset Password',@file_get_contents('./email-templates/forgotpw.html'));
+	if($succ){
+		echo "Successful!";
+	}
+	else{
+		echo "Failed!";
+	}
+}
+
+function getUserLoanByStatus($stat,$userId = 0){
+	$db = DB::getInstance();
+	$conn = $db->getConnection();
+
+	//$sth = $conn->prepare("SELECT * FROM tblLoan WHERE status = ? "); // AND Company = ? 
+	$sth = $conn->prepare("uspLoanGetEmployeebyStatus ?, ?"); // AND Company = ? 
+	$sth->bindParam(1, $userId);
+	$sth->bindParam(2, $stat);
+	$sth->execute();
+
+	$arr = array();
+	while($i = $sth->fetch(PDO::FETCH_ASSOC)){
+		$i['applicationDate'] = date('m-d-Y',strtotime($i['applicationDate']));
+		$i['term'] = $i['numberPaydays'];
+		$arr[] = $i;
+	}
+
+	echo json_encode($arr);
+
+}
+
+function getLoanDetailsByStatus($stat){
+
+	$db = DB::getInstance();
+	$conn = $db->getConnection();
+
+	//$sth = $conn->prepare("SELECT * FROM tblLoan WHERE status = ? "); // AND Company = ? 
+	$sth = $conn->prepare("uspLoanGetbyStatus ?"); // AND Company = ? 
+	$sth->bindParam(1, $stat);
+	$sth->execute();
+
+	$arr = array();
+	while($i = $sth->fetch(PDO::FETCH_ASSOC)){
+		$ud = getUserDetailsById($i['master_id']);
+		$i['employeeName'] = $ud['Name_First']." ".$ud['Name_Last'];
+		$i['applicationDate'] = date('m-d-Y',strtotime($i['applicationDate']));
+		$i['term'] = $i['numberPaydays'];
+		$i['userData'] = $ud;
+		$arr[] = $i;
+	}
+
+	echo json_encode($arr);
+}
+
 switch ($req) {
 	case 'login':
 		$usr = isset($p['username']) ? $p['username'] : "";
 		$pass = isset($p['pass']) ? $p['pass'] : "";
 		$id = login($usr,$pass);
 		if($id){
-			getUserDetailsById($id);
+			echo json_encode(getUserDetailsById($id));
 		}else{
-			echo [false];
+			echo json_encode(array(false));
 		}
 		break;
 	case 'signup':
@@ -102,6 +202,23 @@ switch ($req) {
 		break;
 	case 'get_by_tin':
 		getUserByTIN($p['tin'],$p['birth']);
+		break;
+	case 'forgotpassmail':
+		sendMailForgotPw($p['email']);
+		break;
+	case 'changepass':
+		changePass($p['id'],$p['pass'],$p['newpass']);
+		break;
+	case 'applyloan':
+		//echo json_encode($p['loan']);
+		//break;
+		addLoan($p['loan']);
+		break;
+	case 'get_loan_by_status':
+		getUserLoanByStatus($p['status'],$p['id']);
+		break;
+	case 'hr_get_loan_by_status':
+		getLoanDetailsByStatus($p['status']);
 		break;
 	default:
 		# code...
--- test.php
+++ test.php
@@ -1,34 +1,28 @@
 <?php
 require_once("index.php");
 require_once("mail.php");
+require_once("twilio.php");
 
-
+/**/
 	$db = DB::getInstance();
 	$conn = $db->getConnection();
 
 	$tin = 102843275;
 	$user = "agvil";
 	$pass = "agvilpass";
-	$sth = $conn->prepare("SELECT * From tblMasterList");
+	//$sth = $conn->prepare("SELECT * From tblMasterList");
+	$sth = $conn->prepare("uspLoanGetbyStatus 1");
 	//$sth->bindParam(1, $tin);
 	//$sth->bindParam(1, $user);
 	//$sth->bindParam(2, $pass);
 	$sth->execute();
 	$arr = array();
 	while($i = $sth->fetch(PDO::FETCH_ASSOC)){
+		//$i['applicationDate'] = date('Y-m-d H:i:s',strtotime($i['applicationDate']));
 		$arr[] = $i;
 	}
 
 	echo json_encode($arr);
-/*
-$api = "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";
-	$mail = Mail::getInstance();
 
-	$succ = $mail->send('rsantos@bxbesc.com','santos.ray.rommel@gmail.com','Subject','Predicate <b>bold</b>');
-	if($succ){
-		echo "Successful!";
-	}
-	else{
-		echo "Failed!";
-	}*/
+
 ?>
