fa85fc443be71c4927af1f8f8b66dfdac4b01589
DESKTOP-5KE3RI9\Ray <santos.ray.rommel@gmail.com>
2018-08-08 14:18:31 +0800

    replace test number

--- test.php
+++ test.php
@@ -9,7 +9,7 @@ require_once("twilio.php");
 
 $api = base64_decode("QUM0NGIwMGJiN2MwNzkzMmRhM2M5MjgwY2RmNDViNGVlOQ==");
 $token = base64_decode("NDg5ZjJmMzQ3MzFmNWI2ZTUyMTQ5MzY4ZTQ2NDFkNjY=");
-$number = '+13123455441';
+$number = '+15005550006';
 
 $tw = Twilio::getInstance($api,$token,$number);
 $tw->sms('+639988560027','Hello world');
